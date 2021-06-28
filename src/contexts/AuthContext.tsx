9cd52bfbc7e672f97f8b16f93e31d4de5fe43516
Edmilson Andrade <edmilson.andrad@gmail.com>
2021-06-28 07:15:57 -0300

    teste

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -45,9 +45,9 @@ export function AuthContextProvider(props: AuthContextProviderProps) {
   }, []);
 
   async function signInWithGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    const result = await auth.signInWithPopup(provider);    
+    //const provider = new firebase.auth.GoogleAuthProvider();
+    const provider = new firebase.auth.FacebookAuthProvider;
+    const result = await auth.signInWithPopup(provider);  
 
     if(result.user) {
       const { displayName, photoURL, uid } = result.user;
